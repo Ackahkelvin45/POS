b2dc362f3975446a66c60ea70b5336a14f6758d6
Divine Quansah <divquan@gmail.com>
2023-12-23 10:11:25 +0000

    added login redirect page

--- POS/authentication/views.py
+++ POS/authentication/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,redirect
-from main.models import Domain,Pharmacy
+from django.shortcuts import render, redirect
+from main.models import Domain, Pharmacy
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
@@ -9,84 +9,92 @@ from main.forms import PharmacyForm
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.core.mail import EmailMessage
-from django_tenants.utils import tenant_context,schema_context
+from django_tenants.utils import tenant_context, schema_context
 
 
 # Create your views here.
 
 
-
-
-
-
 def showSignuppage(request):
     context = {
         "userform": UserForm,
-        "pharmacyform":PharmacyForm,
+        "pharmacyform": PharmacyForm,
     }
-    return render(request,'auth/signup.html',context=context)
+    return render(request, "auth/signup.html", context=context)
 
 
 def show(request):
-   
+    return render(
+        request,
+        "auth/email_message.html",
+    )
 
-    return render(request,'auth/email_message.html',)
 
 def signupprocess(request):
     if request.method == "POST":
         userform = UserForm(request.POST)
-        pharmacyform=PharmacyForm(request.POST)
-        password1 = request.POST['password']
-        password2 = request.POST['confirmpassword']
+        pharmacyform = PharmacyForm(request.POST)
+        password1 = request.POST["password"]
+        password2 = request.POST["confirmpassword"]
 
         if password1 == password2:
             if userform.is_valid() and pharmacyform.is_valid():
                 pharmacy = pharmacyform.save(commit=False)
                 user = userform.save()
-                pharmacy.is_mainbranch = True       
+                pharmacy.is_mainbranch = True
                 name = pharmacy.name.replace("-", "")
                 name = name.replace(" ", "")
-                pharmacy.schema_name=name
+                pharmacy.schema_name = name
                 pharmacy.save()
                 user.set_password(password1)
                 user.save()
                 with schema_context(pharmacy.name):
-
                     user.is_admin = True
                     user.is_superuser = True
-                    user.is_staff=True
+                    user.is_staff = True
                     user.save()
-                
-               
+
                 pharmacy.owner = user
                 pharmacy.workers.add(user)
-                pharmacy.save()    
-                domain = Domain(domain=name + ".localhost", tenant=pharmacy, is_primary=True)
+                pharmacy.save()
+                domain = Domain(
+                    domain=name + ".localhost", tenant=pharmacy, is_primary=True
+                )
                 domain.save()
 
-                template = render_to_string("auth/email_message.html", {"name":user.first_name})
+                template = render_to_string(
+                    "auth/email_message.html", {"name": user.first_name}
+                )
                 email = EmailMessage(
-                        "Thank you for choosing samsof pharmacies!",
-                        template,
-                        settings.EMAIL_HOST_USER,
-                        [user.email,]
-
-                    )
+                    "Thank you for choosing samsof pharmacies!",
+                    template,
+                    settings.EMAIL_HOST_USER,
+                    [
+                        user.email,
+                    ],
+                )
                 email.fail_silently = False
                 email.send()
-            
-           
-            
 
-                messages.success(request, "Please allow up to 24 hours for our team to verify your account.")
-                return redirect( 'auth:signup')
+                messages.success(
+                    request,
+                    "Please allow up to 24 hours for our team to verify your account.",
+                )
+                return render(request, "auth/signup_redirect.html")
+                # return redirect("auth:signup")
             messages.error(request, str(userform.errors) + str(pharmacyform.errors))
-            return redirect('auth:signup')
-        messages.error(request, 'Passwords must match')
-        return render('auth:signup')
-    messages.error(request, 'An error occured try again ')
-    return redirect('auth:signup')
+            return redirect("auth:signup")
+        messages.error(request, "Passwords must match")
+        return render("auth:signup")
+    messages.error(request, "An error occured try again ")
+    return redirect("auth:signup")
 
 
+def view_profile(request):
+    if request.user.is_authenticated:
+        return render(request, "auth/userprofile.html")
+    return redirect("auth:login")
 
 
+# def preview_template(request):
+#     return render(request, "auth/signup_redirect.html")
